b91b69a71083a377f6470a3778a0f7583a09a457
Temirlan <tanuku07@gmail.com>
2015-08-06 23:34:05 +0600

    Added picasso lib, showing temp and icon

--- app/src/main/java/com/spalmalo/yweather/activities/MainActivity.java
+++ app/src/main/java/com/spalmalo/yweather/activities/MainActivity.java
@@ -5,29 +5,37 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.spalmalo.yweather.R;
 import com.spalmalo.yweather.http.RestClient;
 import com.spalmalo.yweather.pojos.WeatherResponse;
+import com.squareup.picasso.Picasso;
 
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
 public class MainActivity extends Activity {
-    TextView tv;
+    private TextView weatherDescriptionTextView, degreeTextView;
+    private ImageView iconImageView;
+    private static final String cityName = "Bishkek";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv = (TextView) findViewById(R.id.textView);
+        weatherDescriptionTextView = (TextView) findViewById(R.id.weatherDescriptionTextView);
+        degreeTextView = (TextView) findViewById(R.id.degreeTextView);
+        iconImageView = (ImageView) findViewById(R.id.weatherIconImageView);
 
-        RestClient.get().getWeatherByCityName("Bishkek", new Callback<WeatherResponse>() {
+        RestClient.get().getWeatherByCityName(cityName, new Callback<WeatherResponse>() {
             @Override
             public void success(WeatherResponse weatherResponse, Response response) {
-                tv.setText(weatherResponse.toString());
+                weatherDescriptionTextView.setText(weatherResponse.getWeather().get(0).getDescription());
+                degreeTextView.setText("" + weatherResponse.getMain().getTemp());
+                Picasso.with(getApplicationContext()).load("http://openweathermap.org/img/w/" + weatherResponse.getWeather().get(0).getIcon() + ".png").fit().into(iconImageView);
             }
 
             @Override
@@ -36,7 +44,7 @@ public class MainActivity extends Activity {
             }
         });
     }
-
+//    http://openweathermap.org/img/w/10d.png
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
